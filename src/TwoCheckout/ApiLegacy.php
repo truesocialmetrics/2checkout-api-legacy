d836c4b7dc2f1a65f5e0b8c2151d5a1997289bd8
Rostislav Mykhajliw <necromant2005@gmail.com>
2018-10-29 06:51:20 +0200

    api legacy

--- src/TwoCheckout/ApiLegacy.php
+++ src/TwoCheckout/ApiLegacy.php
@@ -11,4 +11,143 @@ final class ApiLegacy
         $this->vendorCode = $vendorCode;
         $this->secretCode = $secretCode;
     }
+
+    public function rest(array $link_params)
+    {
+        $secret_key = $this->secretCode; //secret key is available on this page https://secure.2checkout.com/cpanel/account_settings.php
+        $base_link = 'https://secure.2checkout.com/action/ise.php';
+        date_default_timezone_set('UTC');
+
+
+        //*********SETTING PARAMETERS*********
+        // $link_params = array();
+        $not_in_hash = array('HASH','INCLUDE_DELIVERED_CODES','INCLUDE_FINANCIAL_DETAILS','INCLUDE_EXCHANGE_RATES','INCLUDE_PRICING_OPTIONS','EXPORT_FORMAT','EXPORT_TIMEZONE_REGION');
+
+        //REQUIRED, CANNOT BE EMPTY:
+        $link_params['MERCHANT'] = $this->vendorCode; //merchant code is available on this page https://secure.2checkout.com/cpanel/account_settings.php
+        $link_params['STARTDATE'] = date("Y-m-d", strtotime('-1 month',strtotime(date('Y').'/'.date('m').'/01'.' 00:00:00'))); //first day from last month
+        $link_params['ENDDATE'] = date("Y-m-d", strtotime('-1 second',strtotime(date('Y').'/'.date('m').'/01'.' 00:00:00'))); //last day from last month
+
+        $link_params['ORDERSTATUS'] = 'ALL'; // replace with any of  ALL, COMPLETE, REFUNDED, UNFINISHED
+        $link_params['REQ_DATE'] = date('YmdHis');
+
+        //CAN BE EMPTY:
+        $link_params['PRODUCT_ID'] = '';
+        $link_params['COUNTRY_CODE'] = '';
+        $link_params['FILTER_STRING'] = '106039576086';
+
+        //REQUIRED, CAN BE EMPTY:
+        $link_params['FILTER_FIELD'] = 'REFNO'; // EMPTY OR: REFNO, REFNOEXT, NAME, EMAIL, COUPONCODE
+
+        //REQUIRED:
+        $link_params['HASH'] = '';
+
+        //OPTIONAL:
+        $link_params['INCLUDE_DELIVERED_CODES'] = '';
+        $link_params['INCLUDE_FINANCIAL_DETAILS'] = '';
+        $link_params['INCLUDE_EXCHANGE_RATES'] = '';
+        $link_params['INCLUDE_PRICING_OPTIONS'] = '';
+        $link_params['EXPORT_FORMAT'] = 'XML'; //possible values CSV or XML -    if you’re using this sample, please specify the desired export format
+        $link_params['EXPORT_TIMEZONE_REGION'] = 'Europe/London';
+
+        //*********GET Base string for HMAC_MD5 calculation:*********
+        $result = '';
+        while(list($key, $val) = each($link_params)){
+            $$key=$val;
+            /* get values */
+            if(!in_array($key,$not_in_hash)){
+
+                if(is_array($val)) $result .= $this->ArrayExpand($val);
+                else{
+                    $size = strlen(StripSlashes($val));
+                    $result .= $size.StripSlashes($val);
+                }
+
+            }
+        }
+
+
+
+        //*********Calculated HMAC_MD5 signature:*********
+        $hash =  $this->hmac($secret_key, $result);
+        $link_params['HASH']=$hash;
+
+        $get_vars = http_build_query($link_params, '', '&');
+
+
+        //*********MAKE POST CALL to get ISE results*********
+        $ch = curl_init($base_link.'?'.$get_vars);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, null);
+        curl_setopt($ch, CURLOPT_POST, FALSE);
+        curl_setopt($ch, CURLOPT_HTTPGET, TRUE);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);  // RETURN THE CONTENTS OF THE CALL
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        $responseData = curl_exec($ch);
+        $headerCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        $contentType = curl_getinfo($ch, CURLINFO_CONTENT_TYPE);
+        //echo 'Curl error: '.curl_error($ch);
+        curl_close($ch);
+
+
+        //*********PROCESS RESULTS*********
+        if ($headerCode==200){
+            // do something with the csv or xml received
+            //the format of the export file is set using $link_params['EXPORT_FORMAT']
+            $exportType = strtolower($link_params['EXPORT_FORMAT']);
+            $headerType = 'Content-type: application/'.$exportType.';charset=UTF-8';
+            $headerDisposition = 'Content-Disposition: attachment; filename="ise.'.$exportType.'"';
+            header($headerType);
+            header($headerDisposition);
+            echo $responseData;
+        }
+        else{
+            //no valid answer received: request period is too big, etc.
+            if ( strpos($contentType,'xml')===false){
+                echo 'Header returned: '.$headerCode;
+                echo $responseData;
+            }
+            else{
+                //YOUR CODE HERE AFTER RECEIVING the xml with one of the codes from Instant Search Export Handbook
+                $xml = $responseData;
+                $xml = simplexml_load_string($xml);
+                $response = array();
+                $i=0;
+                foreach ($xml->children() as $child) {
+                    $response[$i] = $child;
+                    $i++;
+                }
+                echo $xml->asXML();
+            }
+        }
+    }
+
+    //*********FUNCTIONS FOR HMAC*********
+    private function ArrayExpand($array){
+        $retval = "";
+        foreach($array as $i => $value){
+            if(is_array($value)){
+                $retval .= $this->ArrayExpand($value);
+            }
+            else{
+                $size        = strlen(StripSlashes($value));
+                $retval    .= $size.StripSlashes($value);
+            }
+        }
+
+        return $retval;
+    }
+    private function hmac ($key, $data){
+       $b = 64; // byte length for md5
+       if (strlen($key) > $b) {
+           $key = pack("H*",md5($key));
+       }
+       $key  = str_pad($key, $b, chr(0x00));
+       $ipad = str_pad('', $b, chr(0x36));
+       $opad = str_pad('', $b, chr(0x5c));
+       $k_ipad = $key ^ $ipad ;
+       $k_opad = $key ^ $opad;
+       return md5($k_opad  . pack("H*",md5($k_ipad . $data)));
+    }
 }
